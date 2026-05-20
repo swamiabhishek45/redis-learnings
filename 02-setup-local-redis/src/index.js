4c0df960704bed4df4dd704a7831b290f0f1ec5e
swamiabhishek45 <abhishekswami1435@gmail.com>
2026-05-20 15:53:55 +0530

    first commit

---
+++ 02-setup-local-redis/src/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import Redis from 'ioredis'
+import mongoose from 'mongoose'
+
+const app = express();
+
+// redis client
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379')
+
+app.get("/redis", async (req, res)=> {
+    const reply = await redis.ping();
+    res.json({redis: reply});
+})
+
+app.get("/mongo", async (req, res)=> {
+    const url = process.env.MONGO_URL || 'mongodb://localhost:27017/redis_db'
+
+    if(mongoose.connection.readyState === 0) {
+       await mongoose.connect(url);
+    }
+
+    res.json({mongo: 'connected', database: mongoose.connection.name})
+})
+
+app.listen(3000, ()=>{
+    console.log("running");
+    
+})
+
+
